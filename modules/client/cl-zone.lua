21f466207b5524343a425a1951b4bd93ac2fd953
ENT510 <145626625+ENT510@users.noreply.github.com>
2025-03-03 13:05:57 +0100

    Delete modules/client/cl-zone.lua

--- modules/client/cl-zone.lua
+++
@@ -1,7 +0,0 @@
-local Config = require "modules.client.cl-config"
-local Utils  = require "modules.client.cl-utils"
-
-local Cam    = require "modules.client.cl-cam"
-local Shared = require "shared"
-local Zone   = {}
-return Zone
