dbfcf8ba32bb2167aa4ba36e8efb08fcf19d28eb
ENT510 <145626625+ENT510@users.noreply.github.com>
2025-03-03 13:05:24 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,26 +10,16 @@ shared_scripts {
 }
 
 client_scripts {
-    -- Stuff
     'modules/client/cl-config.lua',
     'modules/client/cl-utils.lua',
     'modules/client/cl-cam.lua',
-
-    --Vehicle Shop
-    'modules/client/cl-zone.lua',
     'modules/client/cl-main.lua',
-
-    -- Framework
-    'modules/bridge/client/*.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-
     'modules/server/sv-config.lua',
     'modules/server/sv-callback.lua',
-
-    'modules/bridge/server/*.lua',
 }
 
 
